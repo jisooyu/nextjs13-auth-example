0882e8d28f5a8f5fb22e2e4b1a57d2eeb47ee1fa
jisooyu <jisooyu@hotmail.com>
2023-06-07 08:22:32 +0900

    first commit

---
+++ app/AppBar.js
@@ -0,0 +1,19 @@
+import Link from "next/link"
+
+const AppBar = () => {
+  return (
+      <div className="bg-gradient-to-b from-cyan-50 to-cyan-200 p-2 flex gap-2">
+            <Link className="text-sky-600 hover:text-sky-700" href={"/"}>
+                Home
+            </Link>
+            <Link className="text-sky-600 hover:text-sky-700" href={"/admin"}>
+                Admin
+            </Link>
+            <Link className="text-sky-600 hover:text-sky-700" href={"/admin/panel"}>
+                Panel
+            </Link>
+    </div>
+  )
+}
+
+export default AppBar
