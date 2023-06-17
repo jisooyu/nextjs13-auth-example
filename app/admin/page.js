7c4b3421e999357cce16f6365e2b386056b38f83
jisooyu <jisooyu@hotmail.com>
2023-06-17 09:08:34 +0900

    refining admin page

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,23 +1,23 @@
-'use client'
-import { useState, useEffect } from "react";
-import { useSession } from "next-auth/react";
-import {useRouter} from 'next/navigation'
+'use client';
+// import { useState, useEffect } from "react";
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const Admin = () => {
-  const { data: session } = useSession();
-  const [access, setAccess] = useState(false);
-  const router = useRouter()
-  useEffect(() => {
-    session?.user.id ? (setAccess(true), router.push('/admin')) : (window.alert("SignIn First"),  router.push('/'));
-  }, []);
+	const { data: session } = useSession();
+	// const [access, setAccess] = useState(false);
+	const router = useRouter();
+	// useEffect(() => {
+	//   session?.user.id ? (setAccess(true), router.push('/admin')) : (window.alert("SignIn First"),  router.push('/'));
+	// }, []);
 
-  return (
-    access && (
-      <div className="flex justify-center items-center p-5 text-red-500 text-lg font-bold">
-        Admin Protected Page
-      </div>
-    ) 
-  );
+	return session ? (
+		<div className='flex justify-center items-center p-5 text-red-500 text-lg font-bold'>
+			Admin Protected Page
+		</div>
+	) : (
+		(window.alert('SignIn First'), router.push('/'))
+	);
 };
 
 export default Admin;
